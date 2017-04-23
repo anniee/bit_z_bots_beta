e3075d7388d82f05224a0f427e11722f35f13fc5
Anne Vetto <vettoap@gmail.com>
2017-04-23 12:37:55 -0700

    routing WIP with makestation

--- public/script.js
+++ public/script.js
@@ -1,20 +1,32 @@
 console.log('HI FROM SCRIPT.JS');
 
 angular.module('bitzbotsApp', ['ngRoute'])
-.config(['$routeProvider',
-  function($routeProvider){
+.config(['$routeProvider','$locationProvider',
+  function($routeProvider, $locationProvider){
   $routeProvider
-    .when('/', {
+    .when('/#/', {
         templateUrl: 'index.html',
         controller: 'BitzHomepageController',
-        controllerAs: 'home'
+        controllerAs: 'homeCtrl'
     })
-    // .when("/makestation", {
-    //     templateUrl : "makestation.html"
-    // });
+    .when('/makestation', {
+        templateUrl: 'makestation.html',
+        controller: 'MakestationController',
+        controllerAs: 'makeCtrl'
+    });
+    $locationProvider.html5Mode({
+      enabled: true,
+      requireBase: false
+    });
+
 }])
 .controller('BitzHomepageController', function() {
   var vm = this;
   vm.hello = "hello";
   console.log('hello from bitz controller');
+})
+.controller('MakestationController', function() {
+  var vm = this;
+  vm.hello = "hello";
+  console.log('hello from make controller');
 });
