e8b30fca6e2d1f6f8f71d4401fe4902f1d503a2e
Anne Vetto <vettoap@gmail.com>
2017-05-09 12:23:19 -0700

    Basic workflow without download stl

--- public/script.js
+++ public/script.js
@@ -13,6 +13,11 @@ angular.module('bitzbotsApp', ['ngRoute'])
         templateUrl: 'makestation.html',
         controller: 'MakestationController',
         controllerAs: 'makeCtrl',
+    })
+    .when('/bitzprint', {
+        templateUrl: 'bitzprint.html',
+        controller: 'BitzprintController',
+        controllerAs: 'printCtrl'
     });
     $locationProvider.html5Mode({
       enabled: true,
@@ -20,19 +25,67 @@ angular.module('bitzbotsApp', ['ngRoute'])
     });
 
 }])
+.service('bitzbotService', function() {
+    var myBot = {};
+    var botPhoto = ''
+
+    var makeBot = function(bot) {
+        myBot = bot;
+        myBot = __transformBot(bot);
+        console.log('hello from service', bot);
+    };
+
+    var __transformBot = function(bot) {
+      botNums = [];
+      bot.face = bot.face[(bot.face).length - 1];
+      console.log('bot face num', bot.face);
+      botNums.push(bot.face);
+
+      bot.body = bot.body[(bot.body).length -1];
+      botNums.push(bot.body);
+      bot.legs = bot.legs[(bot.legs).length -1];
+      botNums.push(bot.legs);
+
+      console.log('BOT NUMS', botNums);
+      botPhoto = botNums.join('');
+    }
+
+    var getBot = function(){
+      console.log('BOT PHOTO', botPhoto);
+      return botPhoto;
+        // return myBot;
+    };
+
+    return {
+        makeBot: makeBot,
+        getBot: getBot
+    };
+})
 .controller('BitzHomepageController', function() {
   var vm = this;
   vm.hello = "hello";
   console.log('hello from bitz controller');
 })
-.controller('MakestationController', ['$location', function($location) {
+.controller('MakestationController', ['bitzbotService', '$location', function(bitzbotService, $location) {
   var vm = this;
   vm.hello = "hello";
   vm.bot = {};
   vm.make = function(bot) {
-    console.log('MAKING', bot);
-    // make this route to made place where can download stl and submit email
-    // $location.path('/home');
-  }
+    bitzbotService.makeBot(bot);
+    $location.path('/bitzprint');
+  };
+  // vm.make = function(bot) {
+  //   console.log('MAKING', bot);
+  //   $location.path('/bitzprint');
+  // }
   console.log('hello from make controller');
+}])
+.controller('BitzprintController', ['$scope', 'bitzbotService', function($scope, bitzbotService) {
+  var vm = this;
+  this.bot = bitzbotService.getBot();
+  console.log('THIS BOT', this.bot);
+  console.log('hello from print controller');
+  // console.log('get bot', bitzbotService.getBot());
+  // vm.bot = $scope.bot;
+  // console.log('SCOPE', $scope);
 }]);
